67b3c122d37defd569c4a2cc2733b19fe8f976dd
HamzaB93 <hamzabhatti93@gmail.com>
2013-10-20 10:57:37 +0100

    This is the second exercise in week 4 introtosoftdev.

---
+++ wk4ex2.py
@@ -0,0 +1,8 @@
+#This is a comment for future reference, stating what the code is about.
+#If i start a new line i must put the pound sign.
+
+print "You wont see the comments I have made." 
+
+#see
+
+print "This is all you can see."
