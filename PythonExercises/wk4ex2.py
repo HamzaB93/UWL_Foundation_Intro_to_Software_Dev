5471e89586b4f73d06626a8b66c62f2e4fe87f6e
HamzaB93 <hamzabhatti93@gmail.com>
2013-10-20 16:17:01 +0100

    Repushing same files to test if they can be pushed from a subdirectory

---
+++ PythonExercises/wk4ex2.py
@@ -0,0 +1,19 @@
+print "Second exercise"
+#This is a comment for future reference, stating what the code is about.
+#If i start a new line i must put the pound sign.
+
+print "You wont see the comments I have made." 
+
+#see
+
+print "This is all you can see."
+
+#Testing how to write
+"""
+Many 
+lines 
+of comments
+"""
+print "Tested how to write many lines of comment without using the pund sign over n over."
+print "It worked."
+
---
+++ PythonExercises/wk4ex3.py
@@ -0,0 +1,15 @@
+print "Third exercise"
+#Using python for maths exercise.
+
+print "Basic maths test"
+print 5*2 #The program should give me 10.
+#It worked. YAY.
+
+print "Testing the less than and greater than sign"
+
+print "2 should be less than 5", 2 < 5 #I think it will say true or false.
+
+
+print "Testing another greater and less than sign again"
+print "Is 10 less than 5?" , 10 < 5 #Should say false.
+
