3261632fd6f6178cdb658b25ba07e616b566b399
HamzaB93 <hamzabhatti93@gmail.com>
2013-10-20 11:03:10 +0100

    Added a few comments without usnig the pound sign for multiple lines.

--- wk4ex2.py
+++ wk4ex2.py
@@ -6,3 +6,13 @@ print "You wont see the comments I have made."
 #see
 
 print "This is all you can see."
+
+#Testing how to write
+"""
+Many 
+lines 
+of comments
+"""
+print "Tested how to write many lines of comment without using the pund sign over n over."
+print "It worked."
+
