212d7172ad9e8e9a4ef369bcba0f212882545ad8
HamzaB93 <hamzabhatti93@gmail.com>
2013-10-28 14:23:03 +0000

    Adding wk6ex6

---
+++ Week6/wk6ex6.py
@@ -0,0 +1,30 @@
+x = "There are %d types of people." %10
+binary = "binary"
+do_not = "don't"
+y = "Those who know %s and those who %s." % (binary, do_not)
+
+print x
+print y
+
+print "I said: %r." % x
+print "I also said: '%s'." %y
+
+hilarious = False
+joke_evaluation = "Isn's that joke so funny?! %r"
+
+print joke_evaluation % hilarious
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print w + e
+
+
+print "My own test"
+b = "People find %s really hard." % programming #this doesnt work for somereason??
+do_not = "don't"
+practice = "practice"
+c = "They just %s do enough %s." % (do_not, practice) 
+
+print b
+print c
