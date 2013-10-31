87e1325f93aeccf7839a698608f85306a9bcafe1
HamzaB93 <hamzabhatti93@gmail.com>
2013-10-31 13:31:59 +0000

    uploading an exercise

---
+++ Week5/ex.py
@@ -0,0 +1,33 @@
+print "Integers and floats"
+numlnt = 9
+denlnt = 4
+anslnt = numlnt / denlnt
+print anslnt
+
+ansfloat = float(numlnt)/ denlnt
+print ansfloat
+
+ans2float = 9.0/4; #"dot" is equivalent to float
+print ans2float
+
+
+xlnt = 1
+yfloat = 1.1
+zstr = "Hello!"
+print zstr
+
+
+print "xlnt is of :", type(xlnt)
+
+print type(zstr)
+
+
+
+print " f = c * (9/5)+ 32"
+
+#Input data for variable c
+
+cInputFloat = float(input("Please enter the temperature in celcius"))
+fOutputFloat = float(cInputFloat * (9/5)+32)
+print ("The temperature in fahremheit is", fOutputFloat)
+
