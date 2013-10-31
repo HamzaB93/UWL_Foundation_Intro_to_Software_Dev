9a0f16506412fc53048f2bc359800b849c8a08a6
HamzaB93 <hamzabhatti93@gmail.com>
2013-10-31 14:45:41 +0000

    Uploading wk5ex11.py file

---
+++ Week5/wk5ex11.py
@@ -0,0 +1,9 @@
+print "What is your fist name?",
+firstname = raw_input()
+print "What is your Surname",
+secondname = raw_input()
+print "How old are you?", 
+age = raw_input()
+
+print "So your name is %r %r and you are %r years old." % (
+	firstname, secondname, age)
