3e0eda624358d93e4103c690fe89fd3ff33ae160
HamzaB93 <hamzabhatti93@gmail.com>
2013-11-03 11:51:43 +0000

    uploading wk6ex7

---
+++ Week6/wk6ex7.py
@@ -0,0 +1,21 @@
+print "Why did the %s cross the %s?" % ("chicken", "road")
+print "It wasn't only to get to the %s side" % 'other'
+print "." *10 #<This line means show 10 . 
+print "He really wanted"
+
+end1 = "s"
+end2 = "o"
+end3 = "m"
+end4 = "e"
+end5 = "s"
+end6 = "h"
+end7 = "a"
+end8 = "d"
+end9 = "e"
+end10 = 'lol' 
+
+print end1 + end2 + end3 + end4 ,
+print end5 + end6 + end7 + end8 + end9
+#The comma will keep these two lines on the same line when they are printed.
+print end10
+ 
