fef47270eb474eb2f4069197eb7b120af61e97e8
HamzaB93 <hamzabhatti93@gmail.com>
2013-11-03 17:08:10 +0000

    Uploading two files from week 6, excersises 8 and 9

---
+++ Week6/wk6ex9.py
@@ -0,0 +1,16 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\ nFeb\ nMar\ nApr\ nMay\ nJun\ nJul\ nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months #the months had \ which means it will show me them in a list
+
+#there are three double quotes on the next line.
+print """
+Because there are 3 double quote marks
+I can type as much as I like 
+on as many
+lines
+as 
+I want
+""" #Like comment with the 3 quote marks, but I am printing them now
+
