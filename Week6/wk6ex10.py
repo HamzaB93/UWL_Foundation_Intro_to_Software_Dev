2d7afb96c9f761c09a9223b25f6dc2a142cbab11
HamzaB93 <hamzabhatti93@gmail.com>
2013-11-03 17:27:13 +0000

    Adding wk6 ex10 file

---
+++ Week6/wk6ex10.py
@@ -0,0 +1,16 @@
+tabby_cat = "\tI'm tabbed in." #the \t means the line will be indented 
+persian_cat = "I'm split\non a line." #\n means the string will be put on two lines
+backslash_cat = "I'm \\ a \\ cat." #the string will have slash 
+
+fat_cat = """
+This is a list of random:
+\t* 1
+\t* Ball
+\t* Tomorrow
+\t* Next\n\t* Week
+"""
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
