ccf2fcea439c9aef1a70c0d9c5c45540522ec5d2
HamzaB93 <hamzabhatti93@gmail.com>
2013-11-07 13:02:31 +0000

    If statement excersise

---
+++ Week7/ifstatementex.py
@@ -0,0 +1,12 @@
+print "Why did the golfer carry an extra sock?"
+
+userinput = raw_input ("Please press 'a' for the answer or 'r' for a rephrase ")
+
+if userinput == "a":
+	print "In case he got a whole in one"
+
+elif userinput == "r":
+	print "A golfer carries an extra sock while he was playing, why was that?"
+
+else:
+	print "You need to press a"
