2df4f25688d7b31053766aa964bb2bcb4c9c6b95
HamzaB93 <hamzabhatti93@gmail.com>
2013-11-09 16:47:54 +0000

    adding week 4 practical ex5

---
+++ Week4/Practical4ex5.py
@@ -0,0 +1,9 @@
+print "An area of a triangle is measred by multiplying its height by the width"
+print "Area = Height x Width"
+
+height = float(raw_input("Please enter a Height "))
+width = float(raw_input("Please enter a Width "))
+
+area = height*width
+print "Your area is", area  
+
