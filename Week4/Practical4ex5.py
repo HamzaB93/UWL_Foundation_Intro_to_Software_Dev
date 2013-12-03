4d90ff3cb0014048960f9d66d35d60983bc67247
HamzaB93 <hamzabhatti93@gmail.com>
2013-12-03 10:27:00 +0000

    Uplaoding all of week 4 practicals

--- Week4/Practical4ex5.py
+++ Week4/Practical4ex5.py
@@ -7,3 +7,10 @@ width = float(raw_input("Please enter a Width "))
 area = height*width
 print "Your area is", area  
 
+print "To calculate volume, we need another value, which is length"
+print "We will use your previous numbers, all you need to is add a value for length"
+print "Volume = Width x Height x Length"
+
+length = float(raw_input("Enter your Length "))
+volume = area*length
+print "Your volume is", volume
