696a6583bdb8cea34b1e6a7d1ada3e74b879219b
HamzaB93 <hamzabhatti93@gmail.com>
2013-12-11 11:45:32 +0000

    Uploading week 4 practical ex3 with comments

--- Week4/Practical4ex3.py
+++ Week4/Practical4ex3.py
@@ -1,7 +1,10 @@
 name = raw_input ("What is your name? ")
+# I instructed the user to enter their name. the raw input will be assosciate with the
+# variable name.
 
 print "Hello %r" % name  
-
+# the program will no print the name. %r (raw input) acts as a store within a string. 
+# outside the string the store needs to be defined.
 
 word1 = 'The'
 word2 = 'cat'
@@ -9,7 +12,10 @@ word3 = 'sat'
 word4 = 'on'
 word5 = 'the'
 word6 = 'mat.'
+#i have assigned strings to many varibles
 
 print "%s %s %s %s %s %s" % (
 	word1, word2, word3, word4, word5, word6
 )
+# to the print the varables i used the store %s (string. i then identified what varaiables
+# are being used.
