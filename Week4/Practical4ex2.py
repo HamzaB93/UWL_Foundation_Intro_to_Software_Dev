a2b26d4d220d10a45ad3d33ec30c47c2764f28c7
HamzaB93 <hamzabhatti93@gmail.com>
2013-12-11 11:19:37 +0000

    Uplaoding week 4 practical exercise 2 with comments

--- Week4/Practical4ex2.py
+++ Week4/Practical4ex2.py
@@ -1,7 +1,11 @@
-print "Why did the golfer have an extra sock?"
-userinput = raw_input ("Press 'a' and then enter to see the punch line ")
+print "Why did the golfer have an extra sock?"#The question has been asked.
 
-if userinput == "a":
-	print "Incase he got a whole in one"
-else:
-	print "You had to type 'a' and then enter"
+userinput = raw_input ("Press Enter to see the punch line ")
+#I have instructed the user to press enter to see the punchline.
+#to do this I had created a raw_input instruction before the string.
+
+print "\033[1;32mIncase he got a whole in one\033[1;m"
+#When the user presses enter, they will then see the punchline
+#on either side of the string Incase he got a whole in on, there is a code which will
+#display a different colour when it is ouputted. 
+#the code  \033[1;32m is associate with green.
