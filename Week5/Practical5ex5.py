d2da3440d008bf2e1e985c62e6215382b367e886
HamzaB93 <hamzabhatti93@gmail.com>
2013-12-11 18:08:10 +0000

    Adding the 5th practical from week 5 along with comments

---
+++ Week5/Practical5ex5.py
@@ -0,0 +1,13 @@
+import time # time function is imported
+
+begin = raw_input("Lets see how good you are at guessing 10 seconds. Press enter the start the timer")
+# raw input instructing the user to press enter
+
+start =int( time. time())
+# the time a tthe start of the counter
+
+end = raw_input("Press enter when you think 10 seconds has elapsed")
+# instruction of the user, the count has started
+
+print "You guessed", int(time.time()) - start, "seconds." 
+# the time that the user hit enter is displayed
