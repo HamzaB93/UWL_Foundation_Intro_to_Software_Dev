3a536a7662c1a11cbbb2788e7eda16bacdafa8dc
HamzaB93 <hamzabhatti93@gmail.com>
2013-12-11 11:50:15 +0000

    Adding week 4 practical ex4 with commentst

--- Week4/Practical4ex4.py
+++ Week4/Practical4ex4.py
@@ -1,5 +1,11 @@
 name = raw_input("What is your name?")
 print "Hello %s " % name
+# i instruct the user to enter their name with a raw input. it is assingned to the 
+# variable name. the user is greeted with their name in the string
 
 food = raw_input("What is your favourite food " +name+ "?")
+# the user is then asked another question. the vairable name is added to the string.
+# it is a string within another string
+
 print "I like %s too" % food 
+# the user is replied to with the food they entered menioned
