d6ff793e7842c166be0d44093f236c958221d3f3
HamzaB93 <hamzabhatti93@gmail.com>
2013-12-11 15:29:51 +0000

    Adding week 5 practicalex 2 of week 5 with comments

--- Week5/Practical5ex2.py
+++ Week5/Practical5ex2.py
@@ -1,21 +1,22 @@
 daysperweek = 7 #days
 hoursperday = 24 #hours
 minutesperhour = 60 #mins
+#Formulas below
 
-print """ #Describing the premise
-We have 7 days a week
-Each day lasts 24 hours
-in each hour we have 60 minuts
+minsaday = minutesperhour*hoursperday 
+minsaweek = minsaday*daysperweek
+# all the calculations have been stored
+
+print "We have 7 days a week. Each day lasts 24 hours in each hour we have 60 minuts"
+
+print """
+To calculate how many minuts there are in a week we must do the following:
+First we need to find out how many minutes there are in a day.
+We will multiply the minutes in one hour by the hours in a day.  
 """
-print "To calculate how many minuts there are in a week we must do the following:"
-
-print "First we need to find out how many minutes there are in a day."
-print "We will multiply the minutes in one hour by the hours in a day."  
-print "Which 60 x 24, which gives us:" 
-minsaday = minutesperhour*hoursperday #formula and answer
-print minsaday
+print "Which 60 x 24, which gives us:" , minsaday # displaying one of the calcs
 
 print "All we need to do now is multiply our minuts in a day which is by the days of the week, which is 7"  
-print "Our minuts in a week is:" #formual and answer below
-minsaweek = minsaday*daysperweek
+print "Our minuts in a week are:"
 print minsaweek
+# displaying the answer 
