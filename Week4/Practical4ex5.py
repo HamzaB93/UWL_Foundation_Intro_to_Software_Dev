dc1b08dd717a8ca3dad8b94c3386b44af7fc1489
HamzaB93 <hamzabhatti93@gmail.com>
2013-12-11 15:04:26 +0000

    Adding week 4 practical ex5 with comments

--- Week4/Practical4ex5.py
+++ Week4/Practical4ex5.py
@@ -1,16 +1,17 @@
 print "An area of a rectangle is measred by multiplying its height by the width"
-print "Area = Height x Width"
+print "Area = Height x Width" # quick description of how to calculate area
 
-height = float(raw_input("Please enter a Height "))
-width = float(raw_input("Please enter a Width "))
+height = float(raw_input("Please enter a Height in cm ")) # user inputs as float values
+width = float(raw_input("Please enter a Width in cm "))
 
 area = height*width
-print "Your area is", area  
+print "Your area is", area, "cm squared"   
+#The 2 user inputs are then multiplied together to give the area
 
 print "To calculate volume, we need another value, which is length"
 print "We will use your previous numbers, all you need to is add a value for length"
-print "Volume = Width x Height x Length"
+print "Volume = Width x Height x Length" # another description but for volume
 
 length = float(raw_input("Enter your Length "))
-volume = area*length
-print "Your volume is", volume
+volume = area*length #the user input for length is multiplied by identified area
+print "Your volume is", volume, "cm cubed"
