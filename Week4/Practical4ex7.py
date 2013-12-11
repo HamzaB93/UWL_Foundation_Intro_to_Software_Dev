ab7146e48ccc3790dd2abe1fd9ef518b8939b115
HamzaB93 <hamzabhatti93@gmail.com>
2013-12-11 15:16:52 +0000

    uploading practical ex7 of week 4 with comments

--- Week4/Practical4ex7.py
+++ Week4/Practical4ex7.py
@@ -1,4 +1,7 @@
 firstname = raw_input("What is your first name? ")
 secondname = raw_input("What is your second name? ")
+# the user is instructed to input their first and second name
 
 print (firstname + " "  + secondname + ", ")*3
+# the first and last name are printed. the string will be outputted 3 times
+# as there is a *3, which means repeat 3 times
