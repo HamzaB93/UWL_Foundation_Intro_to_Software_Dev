584ab84f03dc4dfc5741ee8ac60defc4dcee73cb
HamzaB93 <hamzabhatti93@gmail.com>
2013-12-11 15:25:04 +0000

    Adding practical ex1a/b from week 5 with comments

---
+++ Week5/Practical5ex1a.py
@@ -0,0 +1,9 @@
+print "We can work out Distance travelled by multiplying Speed by Time"
+speed = float(raw_input("Please enter your speed in mph "))
+time = float(raw_input("Please enter the amount of time it took in mins "))
+#user is asked to input the speed and time 
+
+distance = speed*(time/60)# the speed is then multiplied by time(which is converted to hrs
+
+print "Your distance travelled in miles was ", distance
+#The answer is then  displayed
---
+++ Week5/Practical5ex1b.py
@@ -0,0 +1,13 @@
+print "I can tell you how fast you need to go within a certain time and distance"
+
+distance = float(raw_input("Enter how far you need to go in miles "))
+time = float(raw_input("Enter the amount of time you have in mins "))
+# user is asked to input time and distance
+
+speed = distance*(time/60)
+# distance is multiplied by time (which has been converted to hrs)
+
+print "You need to travel at ", speed, " mph."
+print "What are you still doing here, GO!"
+# the speed is printed
+
