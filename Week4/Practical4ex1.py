342d22ca3e8810dad1bb2bf8dda581bec01ac2bd
HamzaB93 <hamzabhatti93@gmail.com>
2013-12-11 11:00:58 +0000

    Uploading Week 4 practical exercise 1, containing comments within the code

--- Week4/Practical4ex1.py
+++ Week4/Practical4ex1.py
@@ -1,4 +1,6 @@
 x = """
+#x is my variable, assigned to the varaible are three quote marks
+#the quote marks allow me to enter multiple lines within one variable
 This is 
 a program 
 that prints
@@ -6,5 +8,8 @@ on
 several
 lines
 """
+# everything between the three quote lines will be printed
 
 print x
+#the variable x will be printed
+
