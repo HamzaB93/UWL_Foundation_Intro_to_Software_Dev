895f3a4a461b221556719f58ec15b05591242d0e
HamzaB93 <hamzabhatti93@gmail.com>
2013-12-11 15:11:10 +0000

    Adding practical ex 6 from week 4 with comments

--- Week4/Practical4ex6.py
+++ Week4/Practical4ex6.py
@@ -2,7 +2,11 @@ print "This is simple addition"
 
 a = float(raw_input("Enter any number "))
 b = float(raw_input("Now enter another number "))
+# a and b are variables which require the user to enter any number
+
 c = a + b
+# c is another varaible which adds the two inputs entered and provides an answer
 
 print "Your value is", c
+# c is printed
 
