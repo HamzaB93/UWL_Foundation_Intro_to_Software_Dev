3cae64eb3b6daaeaa2f255a7d5f75987362a6611
HamzaB93 <hamzabhatti93@gmail.com>
2013-12-12 14:30:31 +0000

    Adding practical ex2 from week 2

---
+++ Week8/Practical8ex2.py
@@ -0,0 +1,14 @@
+count = 1 # count starts at 1
+defpass = "changeme" # the desired password is chnageme
+attempt = raw_input("Please enter the default password ")
+# usr asked to enter password
+
+while attempt != defpass:
+	attempt = raw_input("Please enter the default password ")
+	count = count + 1
+	if count > 4:
+		break # count increases when pw is wrong, stops at 4
+if attempt == defpass:
+	print "Password Accepted, it took you " + str(count) + " attempts"
+else:	
+	print "Access denied"
