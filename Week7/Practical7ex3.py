e529b9070c43159b9f7cd5318c267d7a3d2b569a
HamzaB93 <hamzabhatti93@gmail.com>
2013-12-12 08:47:15 +0000

    Adding practical ex3 from week 7 with comments

---
+++ Week7/Practical7ex3.py
@@ -0,0 +1,14 @@
+hrsspnt = int(raw_input("So tell me, how long do you spend on the computer? "))
+# user is asked to enter how long they spend on the computer
+
+if hrsspnt <= 3:
+	print "Sounds reasonable."
+
+elif hrsspnt == 4:
+	print "Do you have time for anything else?"
+
+elif hrsspnt >= 5:	
+	print "Dude you need a life"
+# if statements give a differnt response to the user depending on what they enter
+
+print "Thats all folks"
