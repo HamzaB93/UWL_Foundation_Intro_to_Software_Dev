9b419bdd524b6d17dd06cfb1f71ccd5605aa2264
HamzaB93 <hamzabhatti93@gmail.com>
2013-12-12 08:42:33 +0000

    Adding practical ex2 from week7 with comments

--- Week7/Practical7ex2.py
+++ Week7/Practical7ex2.py
@@ -1,7 +1,8 @@
-import random
-suit = random.randint (1,4)
-card_num = random.randint(1,13)
+import random # importing the random number generator function
+suit = random.randint (1,4) # suit has been given a random number, ranging 1-4
+card_num = random.randint(1,13) # the card number has also been given a random number 1-13 
 
+#if statements which assign a suit name to the the random number
 if suit == 1:
 	suit_name = "Heart"
 if suit == 2:
@@ -12,3 +13,4 @@ if suit == 4:
 	suit_name =  "Spades"
 
 print "Your random card is " + str(card_num) + " " + suit_name
+# the card suit and number are printed for the user
