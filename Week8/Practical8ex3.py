5a463826a05a320426e54746ec2c467d5749e2ac
HamzaB93 <hamzabhatti93@gmail.com>
2013-12-12 09:51:35 +0000

    Uploading ex3 of week8 along with comments

---
+++ Week8/Practical8ex3.py
@@ -0,0 +1,11 @@
+# user is asked to enter a number
+number = float(raw_input("Enter any number "))
+
+fact = 1
+
+while number: # loop 
+	fact = fact*number # the number entered will be timesed by 1 
+	number = number - 1 # 1 taken away from the entered number and looped
+
+print "Your factorial for the number you enetered is", fact
+# the factorial for the entered number is printed
