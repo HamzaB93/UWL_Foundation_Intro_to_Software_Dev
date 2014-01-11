88b24293881c84b6ab646ab6d8df15e729447216
HamzaB93 <hamzabhatti93@gmail.com>
2014-01-11 18:09:34 +0000

    Week10: reuploading ex1 with comments

--- Week10/Practical10ex1.py
+++ Week10/Practical10ex1.py
@@ -1,13 +1,14 @@
 print "\033[1;32mProgram Working\033[1;m"
 
-class BankAccount(object):
-	def _init_ (self, name, accountNumber, balance):
-		self.name_str = name
+class BankAccount(object): # making class
+	def _init_ (self, name, accountNumber, balance): # objects withing the class
+		self.name_str = name # varaibles assocaites with itself
 		self.accountNumber_int = accountNumber
 		self.balance_float = balance
 
+# the functions use the objects from the _init_ 
 	def makeWithdrawal(self, amount):
-		self.balance_float -= amount
+		self.balance_float -= amount # functions using the objects
 
 	def makeDeposit(self, amount):
 		self.balance_float += amount
